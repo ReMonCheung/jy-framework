805a3838d7680c5ac1bca4129340c0f82a71509b
ReMonCheung <736116248@qq.com>
2017-09-12 12:01:25 +0800

    cccccc

--- jy-framework-core/src/main/java/com/jy/framework/core/test/Test.java
+++ jy-framework-core/src/main/java/com/jy/framework/core/test/Test.java
@@ -1,10 +1,21 @@
 package com.jy.framework.core.test;
 
+import java.io.UnsupportedEncodingException;
+
+import com.jy.framework.core.encipher.base64.Base64;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String src = "base64 test 我们记得咖啡碱的";
+		try {
+			String encode = Base64.getEncoder().encodeToString(src.getBytes("UTF-8"));
+			System.out.println(encode);
+			String decode = new String(Base64.getDecoder().decode(encode));
+			System.out.println(decode);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
