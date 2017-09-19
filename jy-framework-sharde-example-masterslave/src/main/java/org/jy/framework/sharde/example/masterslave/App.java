d1efc22dbed9a631b23d0e60d1539d3858b9945a
ReMonCheung <736116248@qq.com>
2017-09-19 16:58:22 +0800

    ccccccccc

---
+++ jy-framework-sharde-example-masterslave/src/main/java/org/jy/framework/sharde/example/masterslave/App.java
@@ -0,0 +1,13 @@
+package org.jy.framework.sharde.example.masterslave;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
